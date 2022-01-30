390a3f10c99e924ef29555d94ca82be7865fedfe
Morten-Mol <modown098@gmail.com>
2022-01-30 12:47:06 +0100

    Comments and refactoring

--- main.py
+++ main.py
@@ -58,6 +58,9 @@ def shuffle_freqs(band1, band2, width, freqs, freq_amp):
         width (float): Width of bands in kHz
         freqs (list): List of all frequencies
         freq_amp(list): List of amplitudes for all frequencies
+
+    Returns:
+        freq_amp(list): Shuffled list of frequency amplitudes
     """
 
     # Scale to kHz
@@ -94,29 +97,29 @@ def shuffle_freqs(band1, band2, width, freqs, freq_amp):
     # BAND 1 CALCULATIONS #
 
     # Translate band frequency to index in frequency list
-    band_freq_index_pos = freqs_num_of_data_points * (band1 / freqs[-1])
+    band1_freq_index_pos = freqs_num_of_data_points * (band1 / freqs[-1])
 
     # Estimate the positive central band frequency boundaries for band 1
-    specific_freq = round(freqs_num_of_data_points + band_freq_index_pos)
+    specific_freq = round(freqs_num_of_data_points + band1_freq_index_pos)
 
     # Estimate the negative central band frequency boundaries for band 1
     band1_freqs_data_points_pos = calc_band_boundaries(specific_freq, freqs_num_of_data_points, width, freqs)
 
     # Estimate the central band frequency in the negative region aswell
-    specific_freq = round(freqs_num_of_data_points - band_freq_index_pos)
+    specific_freq = round(freqs_num_of_data_points - band1_freq_index_pos)
     band1_freqs_data_points_neg = calc_band_boundaries(specific_freq, freqs_num_of_data_points, width, freqs)
 
     # BAND 2 CALCULATIONS #
 
     # Translate band frequency to index in frequency list
-    band_freq_index_pos = freqs_num_of_data_points * (band2 / freqs[-1])
+    band2_freq_index_pos = freqs_num_of_data_points * (band2 / freqs[-1])
 
     # Estimate the positive central band frequency boundaries for band 2
-    specific_freq = round(freqs_num_of_data_points + band_freq_index_pos)
+    specific_freq = round(freqs_num_of_data_points + band2_freq_index_pos)
     band2_freqs_data_points_pos = calc_band_boundaries(specific_freq, freqs_num_of_data_points, width, freqs)
 
     # Estimate the negative central band frequency boundaries for band 2
-    specific_freq = round(freqs_num_of_data_points - band_freq_index_pos)
+    specific_freq = round(freqs_num_of_data_points - band2_freq_index_pos)
     band2_freqs_data_points_neg = calc_band_boundaries(specific_freq, freqs_num_of_data_points, width, freqs)
 
     # Make sure that each window has the same sample length
@@ -150,17 +153,59 @@ def shuffle_freqs(band1, band2, width, freqs, freq_amp):
 # According to https://en.wikipedia.org/wiki/Voice_frequency, the voice frequencies goes from around 0.3 to 3.4 kHz.
 # We should therefore focus our shuffling in that area.
 # Shuffle randomly in voice frequency area
-t_start = time()
-for iterator in range(40000):
-    w = 0.2
-    max_freq = 7
-    start = np.random.uniform(low=w/max_freq) * max_freq
-    end = np.random.uniform(low=w/max_freq) * max_freq
-    while abs(start-end) < w:
-        end = np.random.uniform(low=w/max_freq) * max_freq
+
+def get_shuffle_freqs(w, max_freq):
+    """Get pair of center frequencies to be shuffled with each other.
+
+    A pseudo-random set of two frequency are selected as of now, but in the future
+    this function will select the frequencies as described in an encryption key, which is
+    a list of specific frequencies to be shuffled.
+
+    Args:
+        w (float): Spectral full width of shuffling frequency band
+        max_freq (float): Upper limit of frequencies to be shuffled
+
+    Returns:
+        band1_freq, band2_freq (float): Center frequencies to be shuffled
+    """
+    # Select two pseudo-random center frequncies for shuffling
+    band1_center_freq = np.random.uniform(low=w, high=max_freq)
+    band2_center_freq = np.random.uniform(low=w, high=max_freq)
+
+    # Check that the two selected bands are not too close to each other
+    while abs(band1_center_freq-band2_center_freq) < w:
+        band1_center_freq = np.random.uniform(low=w, high=max_freq)
+        band2_center_freq = np.random.uniform(low=w, high=max_freq)
         print('Random numbers to close to each other')
-    freq_amp = shuffle_freqs(start, end, w, freqs, freq_amp)
+
+    return band1_center_freq, band2_center_freq
+
+
+# Start timer for timing the duration of the shuffling
+t_start = time()
+
+# Number of time shuffling operation is to be run
+n = 40e3
+
+iterator = 1
+while iterator < n+1:
+    # Band freqency width
+    w = 0.2
+
+    # Maximum frequency to be shuffled
+    max_freq = 7
+
+    # Generate random selection of center frequencies for the two bands
+    band1_center_freq, band2_center_freq = get_shuffle_freqs(w, max_freq)
+
+    # Swap the frequency values of the two bands with a given frequency width w
+    freq_amp = shuffle_freqs(band1_center_freq, band2_center_freq, w, freqs, freq_amp)
+
+    # Increase iterator
     print(iterator)
+    iterator += 1
+
+# Calculate the elapsed time for shuffling
 t_end = time()
 t_elapsed = t_end - t_start
 print("Time elapsed: " + str(t_elapsed) + " seconds")
